603e1e713a73a162e941383323307811fc58713f
Ashy4n <mateuszdworowy@gmail.com>
2023-07-03 12:27:47 +0200

    added get img command

---
+++ src/Command/GetImagesCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\ImageProvider;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:get-images',
+    description: 'Add a short description for your command',
+)]
+class GetImagesCommand extends Command
+{
+    public function __construct(private ImageProvider $imageProvider)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+        $img = $this->imageProvider->getImages('2021-07-01');
+        dd($img);
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+
+        return Command::SUCCESS;
+    }
+}
